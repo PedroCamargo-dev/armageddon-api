433bf530e3deea5ef72067f27fd7fca422a8c4fd
Pedro Camargo <pedrocamargo3008@gmail.com>
2023-12-14 16:44:49 -0300

    feat: create gateway socket

---
+++ src/system/system.gateway.ts
@@ -0,0 +1,59 @@
+import {
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+  SubscribeMessage,
+} from '@nestjs/websockets';
+import { SystemService } from './system.service';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class SystemGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly systemService: SystemService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  handleDisconnect(client: any) {
+    console.log('SystemGateway Disconnect', client.id);
+  }
+
+  handleConnection(client: any) {
+    console.log('SystemGateway Connection', client.id);
+  }
+
+  @SubscribeMessage('osInfo')
+  async getOsInfo() {
+    this.server.emit('osInfo', await this.systemService.getOsInfo());
+  }
+
+  @SubscribeMessage('CPU')
+  async getCPU() {
+    setInterval(async () => {
+      this.server.emit('CPU', await this.systemService.getCPU());
+    }, 5000);
+  }
+
+  @SubscribeMessage('Network')
+  async getNetwork() {
+    this.server.emit('Network', await this.systemService.getNetwork());
+  }
+
+  @SubscribeMessage('NetworkStats')
+  async getNetworkStats() {
+    setInterval(async () => {
+      this.server.emit(
+        'NetworkStats',
+        await this.systemService.getNetworkStats(),
+      );
+    }, 1000);
+  }
+
+  @SubscribeMessage('Memory')
+  async getMemory() {
+    setInterval(async () => {
+      this.server.emit('Memory', await this.systemService.getMemory());
+    }, 1000);
+  }
+}
---
+++ src/system/system.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import * as si from 'systeminformation';
+
+@Injectable()
+export class SystemService {
+  async getOsInfo() {
+    const osInfo = await si.osInfo();
+    return osInfo;
+  }
+
+  async getCPU() {
+    const cpu: si.Systeminformation.CpuData = await si.cpu();
+    const currentLoad: si.Systeminformation.CurrentLoadData =
+      await si.currentLoad();
+    return { ...cpu, ...currentLoad };
+  }
+
+  async getNetwork() {
+    const network = await si.networkInterfaces();
+    return network;
+  }
+
+  async getNetworkStats() {
+    const network = await si.networkStats();
+    return network;
+  }
+
+  async getMemory() {
+    const memory: si.Systeminformation.MemData = await si.mem();
+    const memFormated: si.Systeminformation.MemData = { ...memory };
+
+    for (const prop in memFormated) {
+      if (
+        memFormated.hasOwnProperty(prop) &&
+        typeof memFormated[prop as keyof si.Systeminformation.MemData] ===
+          'number'
+      ) {
+        memFormated[prop as keyof si.Systeminformation.MemData] = memFormated[
+          prop as keyof si.Systeminformation.MemData
+        ] as any;
+      }
+    }
+    return memFormated;
+  }
+}
