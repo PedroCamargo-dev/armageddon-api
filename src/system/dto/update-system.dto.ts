cf047b489b9757d426cc4ea054f46999ca09e063
Pedro Camargo <pedrocamargo3008@gmail.com>
2023-12-17 23:46:50 -0300

    format documents

--- src/system/dto/update-system.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSystemDto } from './create-system.dto';
-
-export class UpdateSystemDto extends PartialType(CreateSystemDto) {
-  id: number;
-}
